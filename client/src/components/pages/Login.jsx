ebfcf21c082710a297dbdb232ce65cfa7f3d05c1
Daniel Kalinov <fogm6sujroo5hn@protonmail.com>
2021-03-05 10:32:00 +0200

    Added forms to pages

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className='login'>
+			<form className='form'>
+				<h2>Login</h2>
+				<div className='form-group'>
+					<label htmlFor='email'>Email</label>
+					<input type='text' name='email' className='input' />
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password'>Password</label>
+					<input type='text' name='password' className='input' />
+				</div>
+				<button className='button primary-button'>Log In</button>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/components/pages/Signup.jsx
+++ client/src/components/pages/Signup.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Signup = () => {
-	return <div>Signup</div>;
+	return (
+		<div className='signup'>
+			<form className='form'>
+				<h2>Signup</h2>
+				<div className='form-group'>
+					<label htmlFor='email'>Email</label>
+					<input type='text' name='email' className='input' />
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password'>Password</label>
+					<input type='text' name='password' className='input' />
+				</div>
+				<button className='button primary-button'>Sign Up</button>
+			</form>
+		</div>
+	);
 };
 
 export default Signup;
