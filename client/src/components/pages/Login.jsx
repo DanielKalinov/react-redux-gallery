26c2f8e08f09769e77d24f359c6ec1f2f5387ca6
Daniel Kalinov <fogm6sujroo5hn@protonmail.com>
2021-03-06 09:45:02 +0200

    Added Formik and form validation

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
+	const initialValues = {
+		email: '',
+		password: ''
+	};
+
+	const onSubmit = (values, onSubmitProps) => {
+		onSubmitProps.resetForm();
+	};
+
+	const validationSchema = Yup.object({
+		email: Yup.string().email('Invalid email address').required('Required'),
+		password: Yup.string()
+			.min(8, 'Password must be at least 8 characters long')
+			.required('Required')
+	});
+
 	return (
-		<div className='login'>
-			<form className='form'>
-				<h2>Login</h2>
-				<div className='form-group'>
-					<label htmlFor='email'>Email</label>
-					<input type='text' name='email' className='input' />
-				</div>
-				<div className='form-group'>
-					<label htmlFor='password'>Password</label>
-					<input type='text' name='password' className='input' />
-				</div>
-				<button className='button primary-button'>Log In</button>
-			</form>
-		</div>
+		<Formik
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={onSubmit}
+			validateOnMount>
+			{(formik) => {
+				return (
+					<Form className='form'>
+						<h2>Login</h2>
+						<div className='form-group'>
+							<label htmlFor='email'>Email</label>
+							<Field type='text' name='email' className='input' />
+							<ErrorMessage
+								name='email'
+								component='p'
+								className='form-error-message'
+							/>
+						</div>
+						<div className='form-group'>
+							<label htmlFor='password'>Password</label>
+							<Field type='password' name='password' className='input' />
+							<ErrorMessage
+								name='password'
+								component='p'
+								className='form-error-message'
+							/>
+						</div>
+						<button
+							type='submit'
+							className='button primary-button'
+							disabled={!formik.isValid}>
+							Log In
+						</button>
+					</Form>
+				);
+			}}
+		</Formik>
 	);
 };
 
--- client/src/components/pages/Signup.jsx
+++ client/src/components/pages/Signup.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const initialValues = {
+	email: '',
+	password: ''
+};
+
+const onSubmit = (values, onSubmitProps) => {
+	onSubmitProps.resetForm();
+};
+
+const validationSchema = Yup.object({
+	email: Yup.string().email('Invalid email address').required('Required'),
+	password: Yup.string()
+		.min(8, 'Password must be at least 8 characters long')
+		.required('Required')
+});
 
 const Signup = () => {
 	return (
-		<div className='signup'>
-			<form className='form'>
-				<h2>Signup</h2>
-				<div className='form-group'>
-					<label htmlFor='email'>Email</label>
-					<input type='text' name='email' className='input' />
-				</div>
-				<div className='form-group'>
-					<label htmlFor='password'>Password</label>
-					<input type='text' name='password' className='input' />
-				</div>
-				<button className='button primary-button'>Sign Up</button>
-			</form>
-		</div>
+		<Formik
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={onSubmit}
+			validateOnMount>
+			{(formik) => {
+				return (
+					<Form className='form'>
+						<h2>Signup</h2>
+						<div className='form-group'>
+							<label htmlFor='email'>Email</label>
+							<Field type='text' name='email' className='input' />
+							<ErrorMessage
+								name='email'
+								component='p'
+								className='form-error-message'
+							/>
+						</div>
+						<div className='form-group'>
+							<label htmlFor='password'>Password</label>
+							<Field type='password' name='password' className='input' />
+							<ErrorMessage
+								name='password'
+								component='p'
+								className='form-error-message'
+							/>
+						</div>
+						<button
+							type='submit'
+							className='button primary-button'
+							disabled={!formik.isValid}>
+							Sign Up
+						</button>
+					</Form>
+				);
+			}}
+		</Formik>
 	);
 };
 
